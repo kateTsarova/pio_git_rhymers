ca9316d3580c8622056fbe55814daf046708ab9e
Kate <katerynatsarova@gmail.com>
2021-01-17 20:34:43 +0200

    1.5

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,5 +1,8 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Rhymer, contains array with max size 11
+ */
 public class DefaultCountingOutRhymer {
 
 	private int[] numbers = new int[12];
@@ -14,25 +17,45 @@ public class DefaultCountingOutRhymer {
 
 	public int total = -1;
 
+	/**
+	 * adds value to array numbers
+	 * @param in value to add
+	 */
 	protected void countIn(int in) {
 		if (!isFull())
 			numbers[++total] = in;
 	}
 
+	/**
+	 * checks if array is empty
+	 * @return true if array is empty
+	 */
 	protected boolean callCheck() {
 		return total == -1;
 	}
 
+	/**
+	 * checks if array is full
+	 * @return true if tab is full
+	 */
 	protected boolean isFull() {
 		return total == 11;
 	}
 
+	/**
+	 * returns last added value in array
+	 * @return last value in array
+	 */
 	protected int peekaboo() {
 		if (callCheck())
 			return -1;
 		return numbers[total];
 	}
 
+	/**
+	 * return and delete last value in array
+	 * @return last value in array
+	 */
 	protected int countOut() {
 		if (callCheck())
 			return -1;
